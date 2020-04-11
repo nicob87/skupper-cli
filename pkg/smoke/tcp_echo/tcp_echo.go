ed9dce09357a8029179ffce5ab3d1eb2c1afb586
Nicolas Brignone <nmbrignone@gmail.com>
2020-04-11 11:50:29 -0300

    no second sleep

--- pkg/smoke/tcp_echo/tcp_echo.go
+++ pkg/smoke/tcp_echo/tcp_echo.go
@@ -112,7 +112,6 @@ func (r *SmokeTestRunner) RunTests() {
 
 	r.Pub1Cluster.KubectlExecAsync("port-forward service/tcp-go-echo 9090:9090")
 	r.Priv1Cluster.KubectlExecAsync("port-forward service/tcp-go-echo 9091:9090")
-	time.Sleep(2 * time.Second)
 
 	sendReceive("127.0.0.1:9090")
 	sendReceive("127.0.0.1:9091")
